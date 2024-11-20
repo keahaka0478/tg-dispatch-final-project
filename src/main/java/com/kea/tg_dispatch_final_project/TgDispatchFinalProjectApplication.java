3dd562296c9e1601ee9231d2e582c6d639952844
keahaka0478 <haka0478@stud.kea.dk>
2024-11-20 22:55:18 +0100

    Initial commit. Repository setup

---
+++ src/main/java/com/kea/tg_dispatch_final_project/TgDispatchFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kea.tg_dispatch_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgDispatchFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgDispatchFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kea/tg_dispatch_final_project/TgDispatchFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.tg_dispatch_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgDispatchFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
