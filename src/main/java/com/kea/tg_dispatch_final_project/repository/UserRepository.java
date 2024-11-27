5a4a5f53e7ff3041040b4ea8cebee119112bb34a
keahaka0478 <haka0478@stud.kea.dk>
2024-11-27 15:21:43 +0100

    Rettet manglende @Repository annotation

--- src/main/java/com/kea/tg_dispatch_final_project/repository/UserRepository.java
+++ src/main/java/com/kea/tg_dispatch_final_project/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.kea.tg_dispatch_final_project.repository;
 
 import com.kea.tg_dispatch_final_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
